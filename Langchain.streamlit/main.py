e5cccd31d2b4e8d7be88aeded20cbef2f3893f20
rupeshcodess <153836164+rupeshcodess@users.noreply.github.com>
2024-07-24 11:24:07 +0530

    Update main.py

--- Langchain.streamlit/main.py
+++ Langchain.streamlit/main.py
@@ -7,7 +7,7 @@ import apikey  # Import the file where your API key is stored
 
 load_dotenv()
 
-st.title("Streamlit Chatbot Interface")
+st.title("Streamlit Chatbot")
 
 USER_AVATAR = "👤"
 BOT_AVATAR = "🤖"
